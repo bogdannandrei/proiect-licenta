e682155fafcdb0e769018b69efa4e0b3f41a76e6
bogdannandrei <bogdan-iulian.andrei@github.com>
2023-05-03 16:14:44 +0300

    Worked on the Diary fragment

--- app/src/main/java/com/example/proiectlicenta/Diary.java
+++ app/src/main/java/com/example/proiectlicenta/Diary.java
@@ -7,36 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Diary#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.DateFormat;
+import java.util.Calendar;
+
 public class Diary extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageView prevDayButton;
+    private ImageView nextDayButton;
+    private TextView dateTextView;
+    private Calendar calendar;
 
     public Diary() {
         // Required empty public constructor
     }
-
-    /*
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Diary.
-     */
-    // TODO: Rename and change types and number of parameters
 
     public static Diary newInstance(String phoneNumber) {
         Diary fragment = new Diary();
@@ -50,8 +36,7 @@ public class Diary extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            String phoneNumber = getArguments().getString("phone_number");
         }
     }
 
@@ -59,9 +44,41 @@ public class Diary extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        String phoneNumber = getArguments().getString("phone_number");
+        View view = inflater.inflate(R.layout.fragment_diary, container, false);
 
-        View view =  inflater.inflate(R.layout.fragment_diary, container, false);
+        // Initialize the views
+        prevDayButton = view.findViewById(R.id.prev_day_button);
+        nextDayButton = view.findViewById(R.id.next_day_button);
+        dateTextView = view.findViewById(R.id.date_text_view);
+
+        // Set up the calendar with the current date
+        calendar = Calendar.getInstance();
+        updateDateText();
+
+        // Set up the click listeners for the arrow buttons
+        prevDayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.DAY_OF_MONTH, -1);
+                updateDateText();
+            }
+        });
+        nextDayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+                updateDateText();
+            }
+        });
+
         return view;
+    }
+
+    private void updateDateText() {
+        // Format the current date as a string
+        String dateString = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        // Set the date string to the date text view
+        dateTextView.setText(dateString);
     }
 }
